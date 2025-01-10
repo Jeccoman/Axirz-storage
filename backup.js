89c60452512370479391dcc38ace9243d5efa00c
Jacob Julius <jeccopatrick@outlook.com>
2025-01-10 17:26:57 +0300

    : ubuntu, mac and Win compatibility

--- backup.js
+++ backup.js
@@ -1,25 +1,36 @@
+
 require("dotenv").config();
 const CronJob = require("cron").CronJob;
 const Rsync = require("rsync");
 
+const syncProgram = process.platform === "win32" ? "robocopy" : "rsync";
 
 rsync = new Rsync()
- 
+  .executable(syncProgram)
   .flags("a")
   .source(process.env.SOURCE_DIR)
   .destination(process.env.DESTINATION_DIR);
 
 const job = new CronJob(
-   process.env.CRON_STRING,
+  process.env.CRON_STRING,
   () => {
     rsync.execute((error, code, cmd) => {
+      let result;
+      if (error) {
+
+        result = `Code ${code} ${error?.message}`;
+      } else {
+        result = "Backup complete";
+      }
+
+      const currentDate = new Date().toISOString();
       
-      console.log("backup completed with status code: " + code);
+      process.stdout.write(`${currentDate}: ${result}\n`);
     });
   },
   null,
   true,
-
+  
   "Africa/Nairobi"
 );
 
